1afd7cbadda100ec3bfc380e3a5a00971bff3349
Igor 'Bae <bae.avila@gmail.com>
2018-07-27 17:15:34 -0600

    All Management and Cruds from new requeriment

--- ControlApp.ApiCore/Management/CustomerManagement.cs
+++ ControlApp.ApiCore/Management/CustomerManagement.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ControlApp.DataAccess.Crud;
 using ControlApp.Entities.Objects;
+using log4net;
 
 namespace ControlApp.ApiCore.Management
 {
@@ -14,8 +15,8 @@ namespace ControlApp.ApiCore.Management
         {
             _factory = new CrudCustomer();
         }
-        string ExcepMessage = null; // Management Exception
-        public void CreateCustomer(Customer Obj)
+        private static readonly ILog log = LogManager.GetLogger(System.Environment.MachineName);
+        public void Create(Customer Obj)
         {
             try
             {
@@ -23,10 +24,68 @@ namespace ControlApp.ApiCore.Management
             }
             catch (Exception ex)
             {
-                ExcepMessage = "Logic Error" + ex;
+                log.Error(ex.Message, ex);
             }
         }
-        public List<T> RetrieveAllCustomer<T>()
+        public List<T> SuperRetrieve<T>()
+        {
+            var lst = new List<T>();
+            try
+            {
+                var ret = _factory.SuperRetrieve<Customer>();
+                foreach (var obj in ret)
+                {
+                    lst.Add((T)Convert.ChangeType(obj, typeof(T)));
+                }
+            }
+            catch (Exception ex)
+            {
+
+                log.Error(ex.Message, ex);
+            }
+            return lst;
+        }
+        public List<T> SuperRetrieveById<T>(Customer Obj)
+        {
+            var lst = new List<T>();
+            try
+            {
+                var list = _factory.SuperRetrieveById<Customer>(Obj);
+                if (list.Count > 0)
+                {
+                    foreach (var obj in list)
+                    {
+                        lst.Add((T)Convert.ChangeType(obj, typeof(T)));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message, ex);
+            }
+            return lst;
+        }
+        public List<T> SuperRetrieveByNameDescrip<T>(Customer Obj)
+        {
+            var lst = new List<T>();
+            try
+            {
+                var list = _factory.SuperRetrieveByName<Customer>(Obj);
+                if (list.Count > 0)
+                {
+                    foreach (var obj in list)
+                    {
+                        lst.Add((T)Convert.ChangeType(obj, typeof(T)));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message, ex);
+            }
+            return lst;
+        }
+        public List<T> RetrieveAll<T>()
         {
             var lst = new List<T>();
             try
@@ -39,37 +98,83 @@ namespace ControlApp.ApiCore.Management
             }
             catch (Exception ex)
             {
-
-                ExcepMessage = "Logic Error" + ex;
+                log.Error(ex.Message, ex);
             }
             return lst;
         }
-        public Customer RetrieveCustomer(Customer Obj)
+        public List<T> RetrieveAllByNameDescrip<T>(Customer Obj)
         {
-            var ret = new Customer();
+            var lst = new List<T>();
             try
             {
-                //ret = _factory.RetrieveAll<Customer>(Obj);
+                var list = _factory.RetrieveByName<Customer>(Obj);
+                if (list.Count > 0)
+                {
+                    foreach (var obj in list)
+                    {
+                        lst.Add((T)Convert.ChangeType(obj, typeof(T)));
+                    }
+                }
             }
             catch (Exception ex)
             {
-
-                ExcepMessage = "Logic Error" + ex;
+                log.Error(ex.Message, ex);
             }
-            return ret;
+            return lst;
         }
-        public void UpdateCustomer(Customer Obj)
+        public List<T> RetrieveAllById<T>(Customer Obj)
+        {
+            var lst = new List<T>();
+            try
+            {
+                var list = _factory.RetrieveById<Customer>(Obj);
+                if (list.Count > 0)
+                {
+                    foreach (var obj in list)
+                    {
+                        lst.Add((T)Convert.ChangeType(obj, typeof(T)));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message, ex);
+            }
+            return lst;
+        }
+        public List<T> RetrieveAllByIdUser<T>(Customer Obj)
+        {
+            var lst = new List<T>();
+            try
+            {
+                var list = _factory.RetrieveByIdUser<Customer>(Obj);
+                if (list.Count > 0)
+                {
+                    foreach (var obj in list)
+                    {
+                        lst.Add((T)Convert.ChangeType(obj, typeof(T)));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message, ex);
+            }
+            return lst;
+        }
+        public void Update(Customer Obj)
         {
             try
             {
                 _factory.Update(Obj);
             }
+
             catch (Exception ex)
             {
-                ExcepMessage = "Logic Error" + ex;
+                log.Error(ex.Message, ex);
             }
         }
-        public void DeleteCustomer(Customer Obj)
+        public void Delete(Customer Obj)
         {
             try
             {
@@ -77,7 +182,18 @@ namespace ControlApp.ApiCore.Management
             }
             catch (Exception ex)
             {
-                ExcepMessage = "Logic Error" + ex;
+                log.Error(ex.Message, ex);
+            }
+        }
+        public void Activate(Customer Obj)
+        {
+            try
+            {
+                _factory.Activate(Obj);
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message, ex);
             }
         }
     }
